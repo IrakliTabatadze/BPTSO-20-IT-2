b7d5d1ccd355443795680c159915b971985f6c5e
IrakliTabatadze <irakli.tabatadz.443@gmail.com>
2024-09-12 22:18:55 +0400

    Lecture 1

---
+++ Lecture 01 - Introduction/intro.py
@@ -0,0 +1,61 @@
+#################################################
+# Comment
+#################################################
+
+
+# This is the Comment
+
+"""
+ asasdfadsf
+ asdfasdf
+ asdfasdf
+ asdfasdf
+ asdfasdf
+"""
+
+'''
+kjlkjlkjlkj
+lkjlkjklj
+'''
+
+
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+
+
+#######################################################
+# Data Types
+#######################################################
+
+name1 = 'John'
+name2 = "John"
+numberForMultiplication = 10
+number_for_multiplication = -5
+float_number = 1.5
+float_number2  = .5
+bool_var = True
+bool_var2 = False
+
+
+#######################################################
+# Input/Output
+#######################################################
+
+print(name1)
+print(bool_var)
+print(float_number2)
+
+print(numberForMultiplication + number_for_multiplication)
+print(numberForMultiplication - number_for_multiplication)
+print(numberForMultiplication / number_for_multiplication)
+print(numberForMultiplication * number_for_multiplication)
+print(numberForMultiplication ** number_for_multiplication)
+
+
+number1 = int(input("Please Input number: "))
+number2 = int(input("Please Input number: "))
+
+print(number1 + number2)
